188948cf6c9159df4fa47c20a577e2dcb66e0e17
kennethguerrero <kenetguerrero@gmail.com>
2020-11-21 23:21:45 +0800

    * Info.plist: * HomePage.xaml: * AboutPage.xaml: * MainActivity.cs: * about.json: * EstimateApp.iOS.csproj: * EstimateApp.Android.csproj: * Resource.designer.cs: * MyLogo.png: * MyLogo.png: * MyLogo.png: * MyLogo.png: * MyLogo.png: * 40.png: * 20.png: * 29.png: * 87.png: * 80.png: * 58.png: * 60.png: * 76.png: * 167.png: * 120.png: * 180.png: * 152.png: * 58-1.png: * 1024.png: * 40-2.png: * 40-1.png: * 20-1.png: * 80-1.png: * 120-1.png: * Contents.json: * Contents.json:
    * about.json: Added image animation on About page and added App icon.

--- EstimateApp.Android/MainActivity.cs
+++ EstimateApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace EstimateApp.Droid
 {
-    [Activity(Label = "EstimateApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "EstimateApp", Icon = "@mipmap/MyLogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
